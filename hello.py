0c8e485d92b618bb3f0ece6c835f7c2d32212688
Wesley Sousa <wesley.jlle@gmail.com>
2024-02-03 18:58:19 -0300

    Terceiro commit

--- hello.py
+++ hello.py
@@ -1,8 +1,25 @@
 #!python 3.11.3
 
-'''
-    Programas para ser adicionados
-    neste produto 
-'''
+"""Hello World Multi Linguas
 
-print('Hello, World!')
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Usage:
+"""
+__version__ = "0.0.1"
+__author__  = "Wesley Sousa"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "pt_BR")[:5]
+print(current_language)
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+
+
+print(msg)
